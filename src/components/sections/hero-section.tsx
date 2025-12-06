4ecb254c367c577ad0a716079f920329decf74c8
Perumal <perumal180402@gmail.com>
2025-12-06 04:54:53 +0000

    in the live wallpaper or something. Remove that logo

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -45,7 +45,6 @@ export default function HeroSection({
                 isTransitioning ? 'content-fade-out' : 'content-fade-in'
               )}
             >
-              <Icons.OlipopLogo className="h-auto mb-4" width={130} height={30} />
               <h1
                 className="text-7xl md:text-8xl lg:text-9xl font-black uppercase tracking-tighter"
                 style={{ color: variant.accentColor }}
